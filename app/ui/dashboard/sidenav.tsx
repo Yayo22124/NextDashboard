29871029b3a4848c5df90189c9e16d0d48a7cbb2
Haziel Ortiz <ya-yo22124@outlook.com>
2024-05-04 18:04:09 -0600

    Initial Commit

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,6 +1,6 @@
+import AcmeLogo from '@/app/ui/acme-logo';
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
-import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 
 export default function SideNav() {
